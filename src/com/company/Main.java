1be0baf58e49b6ac5ea00627982f40098b15ce1a
RodrigoPerea <chri65mb@stud.kea.dk>
2022-03-02 09:45:28 +0100

    opret

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    //metoder
+
+
+
+    //udskriv
+
+
+    //ændre
+
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
